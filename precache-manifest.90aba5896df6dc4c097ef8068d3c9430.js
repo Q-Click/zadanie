dab83ca7966b309fe277759927f40c37d025e391
Adam Kukliński <64474051+Q-Click@users.noreply.github.com>
2020-09-01 14:06:14 +0200

    Updates

---
+++ precache-manifest.90aba5896df6dc4c097ef8068d3c9430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee633b826edc4d3de6cec463cc4a31ac",
+    "url": "/zadanie/index.html"
+  },
+  {
+    "revision": "8573366623df62086879",
+    "url": "/zadanie/static/css/main.08c4bd37.chunk.css"
+  },
+  {
+    "revision": "608e466bb5a81fb38cae",
+    "url": "/zadanie/static/js/2.d2c1352c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zadanie/static/js/2.d2c1352c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8573366623df62086879",
+    "url": "/zadanie/static/js/main.fa196841.chunk.js"
+  },
+  {
+    "revision": "4d85865e1159c00adba4",
+    "url": "/zadanie/static/js/runtime-main.ec3ae9e1.js"
+  }
+]);
